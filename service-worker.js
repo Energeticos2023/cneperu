31c4e79851a4a9d474b56f3b21a28a3706ebbef0
Club Rotary Huaraz Colonial <jose.zena.p@uni.pe>
2026-07-21 17:57:35 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='cnep-registro-v4';
+const CACHE='cnep-registro-v5';
 const FILES=['./','./index.html','./manifest.webmanifest','./assets/emblema-cnep.png','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
